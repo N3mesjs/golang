d09d7118a80ce50031a8f8aa4de5cde38faef45b
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2025-04-15 15:39:31 +0200

    .

--- main/main.go
+++ main/main.go
@@ -21,6 +21,32 @@ func main() {
 
 	x, y := coordinate()
 	fmt.Println(x, y)
+
+	for i:=0; i<10; i++ {
+		if i== 5  {
+			continue
+		}
+		fmt.Print(i, "\n")
+	}
+
+	// SLICES AND ARRAYS, slices are dynamic arrays, so they can grow and shrink in size, while arrays are fixed in size.
+	// Slices are more flexible and easier to work with than arrays, so they are used more often in Go. But slices work with arrays in the background, so they are not completely different.
+	// Slices are a reference type, so when you pass a slice to a function, you are passing a reference to the underlying array, not a copy of the array. This means that if you modify the slice, you are modifying the underlying array as well. so if it changes in the funcution, it will change in the main function too.
+	
+	var array = [5]int{1,2,3,6,1}
+	slice := array[1:3]
+	fmt.Println(cap(array), len(array))
+	fmt.Println(cap(slice), len(slice))
+	slice = append(slice, 3, 1, 2)
+	fmt.Println(cap(slice), len(slice))
+
+	sliceN := []int{1,2,3,4,5}
+	fmt.Println(cap(sliceN), len(sliceN))
+	sliceN = append(sliceN, 3, 1, 2)
+	fmt.Println(cap(sliceN), len(sliceN))
+
+	sliceMade := make([]int, 5, 25)
+	fmt.Println(cap(sliceMade), len(sliceMade))
 }
 
 func callback(a, b int) int {
